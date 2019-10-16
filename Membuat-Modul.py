665f39856f242c1737cee35f26a1d9fb7023e19d
nurinayahyusuf <48977773+nurinayahyusuf@users.noreply.github.com>
2019-10-16 19:39:04 +0800

    Create Membuat-Modul.py

---
+++ Membuat-Modul.py
@@ -0,0 +1,21 @@
+#nama File : Anaya
+
+import math
+
+def volumeKubus (R) :
+    return R * R * R
+    
+def luasKubus (R) :
+    return 6 * R * R
+ 
+def volumeBola (r) :
+    return 4/3 * math.pi * r * r
+    
+def luasBola (r) :
+    return 4 * math.pi * r * r
+    
+def volumeTabung (r, t) :
+    return math.pi * r * r * t
+    
+def luasSelimutTabung (r, t) :
+    return 2 * math.pi * r * t
